7244098d51f6b503b029f3e85974939ebb7a7bd2
lucasdulci <luqui00713@gmail.com>
2024-08-26 16:22:29 -0300

    server front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+
+
+const socket = io('https://basic-chat-74h4.onrender.com/');
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
